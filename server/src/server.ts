96fbfe4d944a86c61f91c4e56b04fc7891ca2460
palomaMacedo <macedo.paloma@gmail.com>
2026-03-25 17:53:50 -0300

    ✨ feat: implement get rooms route and database seeding

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,8 @@ import {
 
 import cors from '@fastify/cors'
 import { env } from './env.ts'
+import { get } from 'node:http'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -21,7 +23,7 @@ app.setSerializerCompiler(serializerCompiler)
 app.get('/health', () => {
   return 'OK'
 })
-
+app.register(getRoomsRoute)
 app.listen({ port: env.PORT }).then(() => {
 
 })
